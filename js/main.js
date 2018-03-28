3d0023c92e8dde808342fad9b8b29efaa6a32237
Mladen Zlatanovic <33763725+Mladenkg@users.noreply.github.com>
2018-03-28 22:53:24 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -16,39 +16,3 @@ function oduzmi(x) {
     s -= cena;
     document.getElementById("suma").innerHTML = "Ukupna suma: " + s;
 }
-/*
-
-
-var s = 0;
-
-function dodaj(y) {
-    y = cena;
-    s += cena;
-
-    document.getElementById("suma").innerHTML = "Ukupna suma: " + s;
-}
-
-function oduzmi(y) {
-    y = cena;
-    s -= cena;
-    document.getElementById("suma").innerHTML = "Ukupna suma: " + s;
-}
-var cena = document.getElementById("cena3").innerHTML;
-cena = "cena" + document.getElementById("2");
-cena = new Number(document.getElementById("cena2").innerHTML);
-
-var s = 0;
-
-function dodaj(y) {
-    y = cena;
-    s += cena;
-
-    document.getElementById("suma").innerHTML = "Ukupna suma: " + s;
-}
-
-function oduzmi(y) {
-    y = cena;
-    s -= cena;
-    document.getElementById("suma").innerHTML = "Ukupna suma: " + s;
-}
-*/
