bd0650c5a031bfb1bfcd6c7fd93b32371fb79813
Mladen Zlatanovic <33763725+Mladenkg@users.noreply.github.com>
2018-03-28 17:03:03 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,58 @@
+var cena = document.getElementById["cena1", "cena2", "cena3"];
+cena = "cena" + document.getElementById("1", "2", "3");
+cena = new Number(document.getElementById("cena1", "cena2", "cena3").innerHTML);
+
+var s = 0;
+
+function dodaj(x, y, z) {
+    x = cena[0];
+    y = cena[1];
+    z = cena[2];
+    s += cena;
+
+    document.getElementById("suma").innerHTML = "Ukupna suma: " + s;
+}
+
+function oduzmi(x, y, z) {
+    x = cena;
+    s -= cena;
+    document.getElementById("suma").innerHTML = "Ukupna suma: " + s;
+}
+/*
+var cena = document.getElementById("cena2").innerHTML;
+cena = "cena" + document.getElementById("2");
+cena = new Number(document.getElementById("cena2").innerHTML);
+
+var s = 0;
+
+function dodaj(y) {
+    y = cena;
+    s += cena;
+
+    document.getElementById("suma").innerHTML = "Ukupna suma: " + s;
+}
+
+function oduzmi(y) {
+    y = cena;
+    s -= cena;
+    document.getElementById("suma").innerHTML = "Ukupna suma: " + s;
+}
+var cena = document.getElementById("cena3").innerHTML;
+cena = "cena" + document.getElementById("2");
+cena = new Number(document.getElementById("cena2").innerHTML);
+
+var s = 0;
+
+function dodaj(y) {
+    y = cena;
+    s += cena;
+
+    document.getElementById("suma").innerHTML = "Ukupna suma: " + s;
+}
+
+function oduzmi(y) {
+    y = cena;
+    s -= cena;
+    document.getElementById("suma").innerHTML = "Ukupna suma: " + s;
+}
+*/
